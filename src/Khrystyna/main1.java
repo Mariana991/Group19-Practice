02b5e32f904db57cda6defe547606fffd1554a59
PhKhrystyna <pihurchristina@gmail.com>
2020-11-12 12:59:00 -0500

    new text was added

--- src/Khrystyna/main1.java
+++ src/Khrystyna/main1.java
@@ -3,5 +3,9 @@ package Khrystyna;
 public class main1 {
     public static void main(String[] args) {
         System.out.println("hello, favourite group-19!");
+
+        //Group-19 is the best group!!!
+        //Group-19 will be the best testers!!!
+        //Group-19 is successful!!!
     }
 }
